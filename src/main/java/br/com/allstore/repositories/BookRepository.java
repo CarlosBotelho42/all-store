1449df7866f4beffd789906553fb1a9d3b79bc46
Carlos <carlospiece55@gmail.com>
2024-01-13 18:40:20 -0300

    feat: seprada a camada de model para um outro projeto(all-store-model)

--- src/main/java/br/com/allstore/repositories/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.allstore.repositories;
-
-import br.com.allstore.entities.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
